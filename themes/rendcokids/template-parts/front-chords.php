c2274c06b0e7753838dc07bebbf3207f4bb2706d
evankford <evankerrickford@gmail.com>
2019-08-29 18:47:12 -0400

    Store finalized

--- themes/rendcokids/template-parts/front-chords.php
+++ themes/rendcokids/template-parts/front-chords.php
@@ -1,6 +1,6 @@
 <section id="chords" class="chord-section">
   
-<a href="<?php the_field('chord_file');?>" download>
+<a href="<?php the_field('chord_link');?>" target="_blank" rel="nofollow">
     <?php $chordimg = get_field('chord_image')['id'];
     imgwrap($chordimg, false, '1000');
     ?>
